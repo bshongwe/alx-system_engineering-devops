474a179411163be9c0e9328369a8ec305c8aa1b9
bshongwe <dru.da.beat.hustle@gmail.com>
2024-01-20 02:12:45 -0800

    Task 1 - diff approach

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #!/usr/bin/pup
 # Install a specific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+class { 'python':
+  pip_provider => 'pip3',
+}
+
+python::pip { 'flask':
+  ensure => '2.1.0',
+  pkgname => 'flask',
+  pip_provider => 'pip3',
 }
