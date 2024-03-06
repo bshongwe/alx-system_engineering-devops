a50fc7ff96a8270d8dc20451e3b5f61775ceceaf
bshongwe <dru.da.beat.hustle@gmail.com>
2024-03-05 22:25:02 -0800

    Task 3 - Advanced - <JSON>

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -17,7 +17,7 @@ def count_words(subreddit, word_list, after=None, word_dict=None):
             print(f"{word.lower()}: {count}")
         return
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    url = f"https://www.reddit.com/r/subreddit/hot/.json"
     headers = {"User-Agent": "python:reddit_keyword_counter:v1.0 (by /u/ernest_b_shong)"}
     params = {"limit": 100, "after": after}
     response = requests.get(url, headers=headers, params=params, allow_redirects=False)
