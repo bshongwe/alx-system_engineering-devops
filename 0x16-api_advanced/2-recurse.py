1b1f25d62a8129acaf5539afdee2338b1cb24fd9
bshongwe <dru.da.beat.hustle@gmail.com>
2024-03-07 07:18:44 -0800

    Task 2 - revert

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,7 +6,7 @@ def recurse(subreddit, hot_list=[], after=None, count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = f"https://www.reddit.com/r/subreddit/hot/.json"
     headers = {
-        "User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)"
+        "User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/ernest_b_shong)"
     }
     params = {
         "after": after,
