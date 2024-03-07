7e7c37dc78f25133ddae260891fd1eb0b6ff9eec
bshongwe <dru.da.beat.hustle@gmail.com>
2024-03-07 07:13:34 -0800

    Task 0 - revert

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
 """Query Reddit API for number of subscribers on Reddit subreddit."""
 import requests
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """get number of subscribers to a certain subreddit """
-    client_id = "PpEfMsSxFbxQHsIMzr5faQ"
-    client_secret = "8yTk0bJrYf78NPzdONjt1zWRmYo8SQ"
-    r_usr = "tarekElaasri"
-    r_pass = "(k4r9CQ^^?wN46L"
-    url = "https://www.reddit.com/api/v1/access_token"
-    auth_data = {"grant_type": "password", "username": r_usr,
-                 "password": r_pass}
-    auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
-    header = {'user-agent':
-              'script:subscriber_counter_v0.1 by /u/tarekElaasri'}
-    res = requests.post(url, data=auth_data,
-                        headers=header,
-                        auth=auth)
-
-    if res.status_code == 200:
-        token = res.json()['access_token']
-    else:
-        print(res)
-
-    url = "https://oauth.reddit.com/r/{}/about".format(subreddit)
-    headers = {'Authorization': 'bearer ' + token,
-               'User-Agent':
-               'script:subscriber_counter_v0.1 by /u/tarekElaasri'}
-    res = requests.get(url, headers=headers)
-    if res.status_code == 200:
-        return res.json()['data']['subscribers']
-    else:
+    '''
+        returns the number of subscribers for a given subreddit
+    '''
+    user = {'User-Agent': 'ernest_b_shong'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
+    try:
+        return url.get('data').get('subscribers')
+    except Exception:
         return 0
+
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
