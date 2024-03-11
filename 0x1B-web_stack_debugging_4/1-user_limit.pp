33e219c82d0c12c574f12533957a911e9427fede
bshongwe <dru.da.beat.hustle@gmail.com>
2024-03-10 22:54:59 -0700

    Task 1 - OS DevOpSec config

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# Change OS configuration: allows holberton user login and
+# opens file without any error message.
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
