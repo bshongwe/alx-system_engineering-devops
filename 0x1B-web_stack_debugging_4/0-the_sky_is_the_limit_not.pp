168a4bacce889dd3baead5f41d26bb340612feae
bshongwe <dru.da.beat.hustle@gmail.com>
2024-03-10 22:45:48 -0700

    Task 0 - Nginx fix ---> requests limit fix

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# Fix Nginx using ApacheBench to accept and serve more HTTPS
+# requests to web server
+
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
